05b95eea1126f22ae3e3c269ed580c75db4a1a1c
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-06-27 19:02:37 -0300

    setimo requisito

--- src/challenges.js
+++ src/challenges.js
@@ -34,23 +34,44 @@ function highestCount(arrayNum) {
   let maiorNum = Math.max.apply(null, arrayNum);
   let result = 0;
   for (let index = 0; index < arrayNum.length; index += 1) {
-    if (arrayNum[index] === maiorNum) 
-    result += 1;
+    if (arrayNum[index] === maiorNum) result += 1;
   }
 
   return result;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distancia1 = Math.abs(Math.abs(cat1) - Math.abs(mouse));
+  let distancia2 = Math.abs(Math.abs(cat2) - Math.abs(mouse));
+  if (distancia1 > distancia2) {
+    return 'cat2';
+  } else if (distancia1 < distancia2) {
+    return 'cat1';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+function fizzBuzz(numeros) {
+  let arrayString = [];
+  for (index = 0; index < numeros.length; index += 1) {
+    //  if (numeros[index] % 3 !== 0 && numeros[index] % 5 !== 0) {
+    //     arrayString.push('bug!');
 
+    if (numeros[index] % 3 === 0) {
+      arrayString.push('fizz');
+    } else if (numeros[index] % 5 === 0) {
+      arrayString.push('buzz');
+    } else if (numeros[index] % 3 === 0 && numeros[index] % 5 === 0) {
+      arrayString.push('fizzBuzz');
+    } else {
+      arrayString.push('bug!');
+    }
+    return arrayString;
+  }
+}
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 9
 function encode() {
   // seu código aqui
