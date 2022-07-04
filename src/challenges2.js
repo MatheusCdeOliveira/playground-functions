68c950ab729b2df53209aa8a56a81a85909512e4
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-03 21:16:17 -0300

    requisito 11 feito

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,45 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function confirmaNumOne(numTel) {
+  let numeroValidado = true;
+  for (let valor of numTel) {
+    if (valor < 0 || valor > 9) {
+      numeroValidado = false;
+    }
+  }
+  return numeroValidado;
+}
+
+// function confirmaTamanho(numTel) {
+//   let quantidade = numTel.length;
+//   let validacao = true;
+//   if (quantidade !== 11) {
+//     validacao = false;
+//   }
+//   return validacao;
+// }
+
+function confirmaNumTwo(numTel) {
+  for (let valor2 of numTel) {
+    if (numTel.filter((numeros) => numeros === valor2).length >= 3) {
+      return false;
+    }
+  }
+}
+
+function generatePhoneNumber(numTel) {
+  if (numTel.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (confirmaNumOne(numTel) === false || confirmaNumTwo(numTel) === false) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  let telefone = numTel.join('');
+  let resultado = telefone.replace(/(\d{0})(\d{2})(\d{0})(\d{5})/, '$1($2)$3 $4-'); 
+  // let resultado = telefone.replace(
+  //   /(\d{0})(\d{2})(\d{0})(\d{5}/,
+  //   '$1($2)$3 $4-'
+  // );
+  return resultado;
 }
 
 // Desafio 12
@@ -18,16 +57,16 @@ function triangleCheck(lineA, lineB, lineC) {
 // Desafio 13
 function hydrate(string) {
   let quantidade = 0;
-  for(let valor of string){
-    if (valor >= 1 && valor <= 9){
-      let valorNum = parseInt(valor)
-      quantidade += valorNum
+  for (let valor of string) {
+    if (valor >= 1 && valor <= 9) {
+      let valorNum = parseInt(valor);
+      quantidade += valorNum;
     }
   }
-  if(quantidade === 1){
-  return quantidade + ' copo de água'
+  if (quantidade === 1) {
+    return quantidade + ' copo de água';
   } else {
-    return quantidade + ' copos de água'
+    return quantidade + ' copos de água';
   }
 }
 
